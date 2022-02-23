10b97e8be23e63d7a9c34d7a4889978d00f87556
Lena <elena.pokhvalennaya@gmail.com>
2022-02-23 23:34:12 +0200

    updated task2 and task3

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 // task 1
 
-let date = new Date("May 22, 2002");
-let dateOfMyBirthday = date.getDate();
-let monthOfMyBirthday = date.getMonth() + 1;
-let yearOfMyBirthday = date.getFullYear();
+// let date = new Date("May 22, 2002");
+// let dateOfMyBirthday = date.getDate();
+// let monthOfMyBirthday = date.getMonth() + 1;
+// let yearOfMyBirthday = date.getFullYear();
 
-let myBithday = `${dateOfMyBirthday}.0${monthOfMyBirthday}.${yearOfMyBirthday}`;
-console.log(myBithday);
+// let myBithday = `${dateOfMyBirthday}.0${monthOfMyBirthday}.${yearOfMyBirthday}`;
+// console.log(myBithday);
 
 // task 2
 
 function getDiffDays(start, end) {
   let startDate = new Date(start);
   let endDate = new Date(end);
-  let startDateInMilliseconds = startDate.getTime();
-  let endDateInMilliseconds = endDate.getTime();
-  let diffMilliseconds = endDateInMilliseconds - startDateInMilliseconds;
-  let millisecondsToDays = diffMilliseconds / (1000 * 60 * 60 * 24);
-  if (startDateInMilliseconds > endDateInMilliseconds) {
-    console.error("Error: your start date is later than end");
-    return;
-  }
   if (startDate == "Invalid Date" || endDate == "Invalid Date") {
     console.error("Error: invalid date");
     return;
   }
+  let startDateInMilliseconds = startDate.getTime();
+  let endDateInMilliseconds = endDate.getTime();
+  if (startDateInMilliseconds > endDateInMilliseconds) {
+    console.error("Error: your start date is later than end");
+    return;
+  }
+  let diffMilliseconds = endDateInMilliseconds - startDateInMilliseconds;
+  let millisecondsInDay = 1000 * 60 * 60 * 24;
+  let millisecondsToDays = diffMilliseconds / millisecondsInDay;
   return millisecondsToDays;
 }
 
@@ -37,11 +38,8 @@ console.log(getDiffDays("2021-01-02", "2020-03-15")); // Error: your start date 
 
 function isWeekend(date) {
   let dayOfWeek = new Date(date).getDay();
-  if (dayOfWeek == 0 || dayOfWeek == 6) {
-    return true;
-  } else {
-    return false;
-  }
+  let weekend = dayOfWeek == 0 || dayOfWeek == 6;
+  return weekend;
 }
 
 console.log(isWeekend("2022-02-12")); // true
@@ -50,23 +48,23 @@ console.log(isWeekend("2022-02-09")); // false
 
 // task 4
 
-const person = {
-  fullName: "Sherlock Holmes",
-  address: {
-    street: "Baker Street",
-    city: "London",
-    house: "221b",
-  },
-};
+// const person = {
+//   fullName: "Sherlock Holmes",
+//   address: {
+//     street: "Baker Street",
+//     city: "London",
+//     house: "221b",
+//   },
+// };
 
-const json = JSON.stringify(person);
-console.log(json);
+// const json = JSON.stringify(person);
+// console.log(json);
 
-const newPersonObject = JSON.parse(json);
-console.log(newPersonObject);
+// const newPersonObject = JSON.parse(json);
+// console.log(newPersonObject);
 
-const {
-  fullName,
-  address: { street, city, house },
-} = person;
-console.log(fullName, street, city, house);
+// const {
+//   fullName,
+//   address: { street, city, house },
+// } = person;
+// console.log(fullName, street, city, house);
